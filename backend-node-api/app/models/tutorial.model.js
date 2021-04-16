4065dda14b9b477f4542ef1fedc7f58ac917026f
Sahan Dissanayaka <tsahandisaai@gmail.com>
2021-04-16 09:41:36 +0530

    Add: Node Backend + MySQL + Sequlizer

---
+++ backend-node-api/app/models/tutorial.model.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, Sequelize) => {
+    const Tutorial = sequelize.define("tutorials", {
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      published: {
+        type: Sequelize.BOOLEAN
+      },
+    }, {
+    // disable the modification of table names; By default, sequelize will automatically
+    // transform all passed model names (first parameter of define) into plural.
+    // if you don't want that, set the following
+    freezeTableName: true,
+    });
+  
+    return Tutorial;
+};
