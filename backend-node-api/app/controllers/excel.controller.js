9bf016ba3f11c3607edb0dc643a322210d49acaf
Sahan Dissanayaka <tsahandisaai@gmail.com>
2021-04-26 18:42:56 +0530

    Backend: download data from to excel

--- backend-node-api/app/controllers/excel.controller.js
+++ backend-node-api/app/controllers/excel.controller.js
@@ -2,6 +2,7 @@ const db = require("./../models/index.js");
 const Subject = db.subjects;
 
 const readXlsxFile = require("read-excel-file/node");
+const excel = require("exceljs");
 
 const upload = async (req, res) => {
   try {
@@ -65,7 +66,51 @@ const getTutorials = (req, res) => {
     });
 };
 
+const download = (req, res) => {
+  Subject.findAll().then((objs) => {
+    let tutorials = [];
+
+    objs.forEach((obj) => {
+      tutorials.push({
+        id: obj.id,
+        title: obj.title,
+        description: obj.description,
+        published: obj.published,
+        userId: obj.userId
+      });
+    });
+
+    let workbook = new excel.Workbook();
+    let worksheet = workbook.addWorksheet("Tutorials");
+
+    worksheet.columns = [
+      { header: "Id", key: "id", width: 5 },
+      { header: "Title", key: "title", width: 25 },
+      { header: "Description", key: "description", width: 25 },
+      { header: "Published", key: "published", width: 10 },
+      { header: "UserId", key: "userId", width: 10},
+    ];
+
+    // Add Array Rows
+    worksheet.addRows(tutorials);
+
+    res.setHeader(
+      "Content-Type",
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    );
+    res.setHeader(
+      "Content-Disposition",
+      "attachment; filename=" + "data.xlsx"
+    );
+
+    return workbook.xlsx.write(res).then(function () {
+      res.status(200).end();
+    });
+  });
+};
+
 module.exports = {
   upload,
+  download,
   getTutorials,
 };
